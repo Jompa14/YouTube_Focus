c741c8b3f34b0f161d4e98e3f3e6015219131e6e
andreicosta <acosta@inf.ufpel.edu.br>
2019-09-09 20:51:49 -0300

    Testes de classes

--- content_scripts/content_focus.js
+++ content_scripts/content_focus.js
@@ -1,1 +1,16 @@
-console.log('YouTube_Focus');
+if (!document.getElementById('youtubeFocusBrain')) {
+	var style = document.createElement('style');
+	style.type = 'text/css';
+	style.id = 'youtubeFocusBrain';
+	style.innerHTML = '.style-scope.ytd-compact-video-renderer { display: none; }';
+	style.innerHTML += '.style-scope.ytd-compact-radio-renderer { display: none; }';
+	style.innerHTML += '.style-scope.ytd-compact-playlist-renderer { display: none; }';
+	style.innerHTML += '.ytp-endscreen-content { display: none; }';
+	style.innerHTML += '.style-scope.ytd-shelf-renderer { display: none; }';
+	document.head.appendChild(style);
+
+	/*var autoplay = document.getElementById('toggle');
+	if (autoplay && autoplay.checked) {
+		autoplay.checked = false;
+	}*/
+}
