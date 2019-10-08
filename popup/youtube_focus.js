6c88e5f9c218ec27109fbca0c1da534625ac2177
João Paulo Buchholz <51836804+Jompa14@users.noreply.github.com>
2019-10-08 16:08:23 -0300

    Update youtube_focus.js

--- popup/youtube_focus.js
+++ popup/youtube_focus.js
@@ -5,11 +5,15 @@ document.addEventListener("click", function(e) {
 
   var chosenMode = e.target.textContent;
   
+  // para injetar o content script na página:
   chrome.tabs.executeScript(null, {
     file: "/content_scripts/content_focus.js" 
-  });
+  }); 
 
+ //para obter a aba ativa:
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    /*para enviar uma mensagem para o content scripts executando na aba ativa. 
+    A mensagem irá conter as propriedades do modo escolhido:*/
     chrome.tabs.sendMessage(tabs[0].id, {mode: chosenMode});
   });
 
