4e6f7649640795ed0f13897b49cbc6fd46ad17e0
Jompa14 <joao.buchholz@gmail.com>
2023-02-20 17:30:01 -0300

    focus mode finally working

--- inject.js
+++ inject.js
@@ -30,7 +30,7 @@ function brain() {
   if (document.getElementById('youtubeFocusBrain')) {
     document.head.removeChild(document.getElementById('youtubeFocusBrain'));
   }
-
+  
   var style = document.createElement('style');
   style.type = 'text/css';
   style.id = 'youtubeFocusBrain';
@@ -64,18 +64,16 @@ function focus() {
   style.id = 'youtubeFocusBrain';
 
   style.innerHTML = `
-    *[page-subtype~="home"],
-    *[href*="/feed/trending"],
-    .ytp-endscreen-content,
-    #related #contents,
-    #related #upnext,
-    #related ytd-compact-video-renderer:not(:first-child):not(:nth-child(2)):not(:nth-child(3)):not(:nth-child(4)),
-    #related ytd-compact-playlist-renderer,
-    #related ytd-compact-radio-renderer:not(:first-child):not(:nth-child(2)):not(:nth-child(3)):not(:nth-child(4)),
-    .watch-sidebar-body,
-    #feed-main-what_to_watch{
-      display: none !important;
-    }
+      *[page-subtype~="home"],
+      *[href*="/feed/trending"],
+      .ytp-endscreen-content,
+      ytd-compact-video-renderer:nth-child(n+4),
+      #related ytd-compact-playlist-renderer,
+      #related ytd-compact-radio-renderer,
+      .watch-sidebar-body,
+      #feed-main-what_to_watch{
+        display: none !important;
+      }
   `;
 
   document.head.appendChild(style);
